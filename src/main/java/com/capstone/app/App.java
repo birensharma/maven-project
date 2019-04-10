97b7dbcc151608ca6d66d50c7ee0cbd88534177a
birensharma <birensharma@st.niituniversty.in>
2019-04-11 01:40:23 +0530

    Main

--- src/main/java/com/capstone/app/App.java
+++ src/main/java/com/capstone/app/App.java
@@ -1,13 +1,9 @@
 package com.capstone.app;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "This is a Maven project" );
     }
 }
