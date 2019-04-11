ec12266fa3b03cff04fbc5ef8dfbf0456c523353
mittalhitesh1998 <mittalhitesh1998@gmail.com>
2019-04-11 12:26:16 +0530

    hidsh

--- src/main/java/com/capstone/app/App.java
+++ src/main/java/com/capstone/app/App.java
@@ -4,12 +4,10 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
+
         System.out.println( "hello sir this is mid 2 " );
-=======
         System.out.println( "This is a Maven project" );
         for(int i=0;i<10;i++)
             System.out.println(i);
->>>>>>> 96de029c44b7e545ab3bb5e308e9afcdde4e0e09
     }
 }
